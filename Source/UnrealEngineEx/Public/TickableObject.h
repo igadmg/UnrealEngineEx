f815aec75e68d3d809cf931a41cc42fc45ee4875
Igor Karatayev <igor.cwer@gmail.com>
2019-04-29 23:31:46 +0300

    tickable bp.

--- Source/UnrealEngineEx/Public/TickableObject.h
+++ Source/UnrealEngineEx/Public/TickableObject.h
@@ -4,7 +4,7 @@
 
 
 
-UCLASS()
+UCLASS(blueprintable, BlueprintType)
 class UNREALENGINEEX_API UTickableObject : public UObject
 {
 	GENERATED_BODY()
@@ -12,7 +12,7 @@ class UNREALENGINEEX_API UTickableObject : public UObject
 
 
 public:
-	UFUNCTION(Category = "UnrealEngineEx: Tickable", BlueprintNativeEvent)
+	UFUNCTION(Category = "UnrealEngineEx: Tickable", BlueprintCallable, BlueprintNativeEvent)
 	void Tick(float DeltaSeconds);
 
 
